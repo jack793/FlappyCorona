b1613337007a6b56a8f38602834a52807e6b7305
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2017-12-15 18:32:42 +0100

    ADD main.lua and menu.lua (incomplete) files

---
+++ main.lua
@@ -0,0 +1,21 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Giacomo
+-- Date: 15/12/17
+-- Time: 17:51
+-- To change this template use File | Settings | File Templates.
+--
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- include the Corona "composer" module
+local composer = require "composer"
+
+-- load menu screen
+composer.gotoScene( "menu" )
---
+++ menu.lua
@@ -0,0 +1,28 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Giacomo
+-- Date: 15/12/17
+-- Time: 18:23
+-- To change this template use File | Settings | File Templates.
+--
+-----------------------------------------------------------------------------------------
+--
+-- menu.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- include the Corona "composer" module and add this scene
+local composer = require "composer"
+local scene = composer.newScene()
+
+-- include physics and data deps
+local physics = require "physics"
+physics.start()
+
+local mydata = require( "mydata" )
+
+------------------------------------ MENU FUNCTIONS -------------------------------------
+
+
+
+
