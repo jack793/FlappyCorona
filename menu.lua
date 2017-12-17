e750917dea5b204ca79cfe6ce6b3159540d23720
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2017-12-17 13:38:54 +0100

    Update menu

--- menu.lua
+++ menu.lua
@@ -30,9 +30,24 @@ function scene:create(event)
     local menuScene = self.view
 
     -- Add object, listeners and interacions to menuScene
+
+    -- Background
     background = display.newImageRect("bckgr.png",900,1425)
     background.anchorX = 0.5
     background.anchorY = 1
+    background.x = display.contentCenterX
+    background.y = display.contentHeight
+    menuScene:insert(background)
+
+    -- Title
+    title = display.newImageRect("title.png",500,100)
+    title.anchorX = 0.5
+    title.anchorY = 0.5
+    title.x = display.contentCenterX - 80
+    title.y = display.contentCenterY
+    sceneGroup:insert(title)
+
+
 end
 
 
