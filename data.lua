2dc3f67d06d8f94cf2a9afe9f2f5cd54baf74e3e
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2017-12-18 18:23:34 +0100

    Add DATA MODULE, ready for future local SCORE MODULE data file

---
+++ data.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Giacomo
+-- Date: 18/12/17
+-- Time: 18:18
+-- To change this template use File | Settings | File Templates.
+--
+-----------------------------------------------------------------------------------------
+--
+-- data.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- used for handling global data
+-- that we wish to pass between multiple scenes
+local M = {}
+return M
+
